7b68fefad5ac4d2d65e99f372b86b79ea4e0b837
sathish19961412 <sathishfreelanc5.com>
2024-07-30 08:30:59 -0400

    java basic variables,datatypes

---
+++ Datatypes.java
@@ -0,0 +1,15 @@
+public class Datatypes
+{
+    public static void main(String[] args){
+
+        // int age=4000;
+        // byte age=18;
+        // short age=18;
+        // float mark=95.5F;
+        //  double mark=95.588888;
+        // boolean presence=true;
+        char grade = 's';
+        // System.out.println("It is a interger Value",age);
+        System.out.println(grade);
+    }
+}
